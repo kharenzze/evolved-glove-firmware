9d78da4a3d8331d82d52bded00d25043a556647c
kharenzze <str237@gmail.com>
2017-08-31 17:59:03 +0000

    refactor test code

--- src/test.cpp
+++ src/test.cpp
@@ -13,16 +13,20 @@ using namespace std;
 using namespace ABElectronics_CPP_Libraries;
 using namespace EGUtils;
 
-int main (int argc, char **argv) {
+void testADCPI() {
 	ADCPi adc(0x68, 0x69, 18);
 	adc.set_conversion_mode(1);
 	adc.set_pga(4);
 
-	while (1){
+	while (1) {
 		clearscreen();
 		printf("Pin 1: %G \n", adc.read_voltage(1)); // read from channel 1
 		//printf("Pin 1: %d \n", adc.read_raw(1)); // read from channel 1
 		usleep(200000); // sleep 0.2 seconds
 	}
+}
+
+int main (int argc, char **argv) {
+	testADCPI();
 	return 0;
 }
